0f76f74feb4aa70c0d4dfbdb1131e1b3a7289f2e
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-06-11 19:22:52 +0530

    Fix build errors - frontend

--- kahoot-frontend/src/app/(public)/test/correct/page.tsx
+++ kahoot-frontend/src/app/(public)/test/correct/page.tsx
@@ -2,7 +2,7 @@ import Correct from "@/components/correct";
 import React from "react";
 
 const page = () => {
-  return <Correct />;
+  return <Correct score={100} answerStreak />;
 };
 
 export default page;
